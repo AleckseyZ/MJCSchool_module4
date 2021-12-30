76e7eb5ec0880d04f8591485140845e943ae2fad
AleckseyZ <66429114+AleckseyZ@users.noreply.github.com>
2021-12-30 11:51:33 +0300

    configuration fix

--- dataAccess/src/main/java/com/epam/esm/zotov/mjcschool/dataaccess/DataAccessConfig.java
+++ dataAccess/src/main/java/com/epam/esm/zotov/mjcschool/dataaccess/DataAccessConfig.java
@@ -40,8 +40,6 @@ public class DataAccessConfig {
     private String password;
     @Value("${test.url}")
     private String testUrl;
-    @Value("${test.tempUrl}")
-    private String tempUrl;
     @Value("${hibernate.scanPackage}")
     private String scanPackage;
     @Value("${hibernate.dialectPropertyName}")
